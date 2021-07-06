8012bb217cd68cdb8b75e8a761a9654df9cf1387
Caroline Lu <42614552+carolinedlu@users.noreply.github.com>
2021-07-06 17:30:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,4 @@ import streamlit as st
 import socket
 import os
 
-st.write(os.system('hostname'))
-
-print(socket.gethostname())
-st.write(socket.gethostname())
-
-
 st.write(os.environ.get("STREAMLIT_HOST", ""))
